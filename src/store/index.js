1eb66582e91f2e40a33d88e1cf49b375d519bd9d
Kasope Johnson <kasopej@gmail.com>
2022-10-10 20:52:07 +0100

    upgrades vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,47 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from "vuex";
 import { todoData } from "../shared/data";
 import { parseData } from "../shared/data-service";
 
-Vue.use(Vuex)
-
 const state = {
-  todoDataStore: [], error: false,
+  todoDataStore: [],
+  error: false,
 };
 const mutations = {
   populateStoreData(state, data) {
     state.todoDataStore = data;
-  }
+  },
 };
 const actions = {
   letStoreGetData({ commit }) {
     const data = parseData(todoData);
-    commit('populateStoreData', data);
+    commit("populateStoreData", data);
   },
   updateData(context, data) {
-    const index = todoData.findIndex(h => h.id == data.id);
+    const index = todoData.findIndex((h) => h.id == data.id);
     todoData.splice(index, 1, data);
-    context.dispatch('getData');
+    context.dispatch("getData");
   },
   addData(context, data) {
-    if (todoData.findIndex(h => h.id == data.id) === -1) {
+    if (todoData.findIndex((h) => h.id == data.id) === -1) {
       todoData.push(data);
-      context.dispatch('getData');
+      context.dispatch("getData");
       context.state.error = false;
+    } else {
+      context.state.error = true;
     }
-    else { context.state.error = true; }
-  }
+  },
 };
 const getters = {
-  getEntryById: state => id => {
-    console.log('Getting unique data');
-    return state.todoDataStore.find(h => h.id == id)
+  getEntryById: (state) => (id) => {
+    console.log("Getting unique data");
+    return state.todoDataStore.find((h) => h.id == id);
   },
 };
 
-export default new Vuex.Store({
+export default createStore({
   state,
   mutations,
   actions,
   getters,
-})
+});
