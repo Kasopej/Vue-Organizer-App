8fd50b9451eed52f483e6f1ecbf9db3e50d563be
Kasope Johnson <kasopej@gmail.com>
2022-10-10 20:41:07 +0100

    upgrades vue router & instantiates it in app

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import MainView from '../views/main-view.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHistory } from "vue-router";
+import MainView from "../views/main-view.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'MainView',
-    component: MainView
+    path: "/",
+    name: "MainView",
+    component: MainView,
   },
   {
-    path: '/edit-todo/:id',
-    name: 'EditTodo',
-    props: r => ({ id: parseInt(r.params.id) }),
+    path: "/edit-todo/:id",
+    name: "EditTodo",
+    props: (r) => ({ id: parseInt(r.params.id) }),
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "edit-mode" */ '../views/edit-todo.vue')
+    component: () =>
+      import(/* webpackChunkName: "edit-mode" */ "../views/edit-todo.vue"),
   },
   {
-    path: '/add-todo',
-    name: 'AddTodo',
-    component: () => import(/* webpackChunkName: "edit-mode" */ '../views/add-todo.vue')
-  }
-]
+    path: "/add-todo",
+    name: "AddTodo",
+    component: () =>
+      import(/* webpackChunkName: "edit-mode" */ "../views/add-todo.vue"),
+  },
+];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
